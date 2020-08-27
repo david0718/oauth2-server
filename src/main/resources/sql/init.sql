9b702d7fc0dea9cf1de985b8ae0ee97d3929784c
david0718 <david0718@qq.com>
2020-08-27 19:49:59 +0800

    add manifest file

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -2,7 +2,7 @@ INSERT INTO oauth_client_entity (id, client_id, application_name, client_secret,
                                  scope, web_server_redirect_uri, access_token_validity, refresh_token_validity, remarks)
 VALUES (1, 'SampleClientId', 'SampleClientId 测试应用', '$2a$10$gcrWom7ubcRaVD1.6ZIrIeJP0mtPLH5J9V/.8Qth59lZ4B/5HMq96',
         'authorization_code,refresh_token,password', 'ROLE_TRUSTED_CLIENT', 'user_info',
-        'http://client.sso.com/login/oauth2/code/sso-login', 7200, 2592000, '测试明文:tgb.258');
+        'https://identity-server.apps.dhccloud.com.cn/login/oauth2/code/sso-login', 7200, 2592000, '测试明文:tgb.258');
 
 INSERT INTO scope_definition_entity (id, scope, definition)
 values (1, 'user_info', '昵称、头像、性别信息');
@@ -19,4 +19,9 @@ VALUES (1, 'zhangsan', '$2a$10$gcrWom7ubcRaVD1.6ZIrIeJP0mtPLH5J9V/.8Qth59lZ4B/5H
 insert into user_account_entity_roles(user_id, role_id)
 values (1, 1),
        (3, 3);
+
+
+# init  sample data
+use oauth2_server;
+update oauth_client_entity set web_server_redirect_uri='https://identity-server.apps.dhccloud.com.cn/login/oauth2/code/sso-login' where id=1
 
